df356dea6fe778b86ffc4fdb1b0dce1d5077f8ea
Taylor Powell <taylor.powell2001@gmail.com>
2021-10-25 13:06:13 -0600

    initial commit to submit

---
+++ articles/admin.py
@@ -0,0 +1,11 @@
+# articles/admin.py
+from django.contrib import admin
+from .models import Article, Comment
+class CommentInline(admin.TabularInline): # new
+    model = Comment
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
